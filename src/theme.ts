f1e7f33aa8a67db881dcea4d253df7a8fec753e4
horikawa <nesheep.dream@gmail.com>
2022-04-08 01:59:01 +0900

    update style

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,23 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
+  palette: {
+    background: {
+      default: '#79a467',
+    }
+  },
   typography: {
     fontFamily: ['Roboto', 'Noto Sans JP', 'Helvetica', 'Arial', 'sans-serif'].join(','),
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        '::-webkit-scrollbar': { width: 10 },
+        '::-webkit-scrollbar-thumb': {
+          backgroundColor: 'rgba(0, 0, 0, 0.3)',
+          borderRadius: 5,
+        },
+      },
+    },
   },
 });
